10960c448e3450afbe4db076ad9ad7b16de088ad
Gizzat <gizzat.tazabekov@gmail.com>
2021-06-13 16:13:58 -0700

    rooms work

--- app/routes/rooms/room.js
+++ app/routes/rooms/room.js
@@ -1,5 +1,10 @@
 import Route from '@ember/routing/route';
 
 export default class Room extends Route {
-
+  model(params, transition) {
+    return {
+      params,
+      transition
+    };
+  }
 }
